6683a5b593c956ff9124320d7e0784b25ef27a61
Tomas Holtz <tomiholtz@gmail.com>
2024-01-25 23:04:14 -0300

    Desktop Home

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,7 +4,7 @@ import { InstagramLogo, TwitterLogo, GitHubLogo } from './MediaIcons'
 export function Header() {
 
     return (
-        <header className="px-[72px] justify-between flex pt-[53px] items-center ">
+        <header className="px-[72px] h-[15vh] justify-between flex pt-[53px] items-center ">
             <HeaderLinks />
             <div className="flex flex-row">
                 <GitHubLogo />
