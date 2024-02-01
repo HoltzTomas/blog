83aa8c29892c5751e77d5183856c1176c24a48a6
Tomas Holtz <tomiholtz@gmail.com>
2024-02-01 10:10:01 -0300

    New bio in home

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,15 @@ import { AvatarLogo } from "./components/AvatarLogo";
 export default function Home() {
   return (
     <div className="overflow-y-scroll px-[16px] md:px-[53px]">
-      <div className="flex flex-col pb-[2rem] h-[85vh]">
-        <h1 className={`mt-10 md:mt-20 text-center align-center text-[48px] md:text-[5rem] ${NeueMachinaUltraBold.className}`}>TOMAS HOLTZ</h1>
-        <p className={`mt-[41px] text-[18px] text-center align-center ${NeueMachinaRegular.className}`}>Esto podria haber sido una cuenta de medium, pero bueno.</p>
+      <div className="flex flex-col pb-[2rem]">
+        <h1 className={`mt-10 md:mt-15 text-center align-center text-[48px] md:text-[5rem] ${NeueMachinaUltraBold.className}`}>TOMAS HOLTZ</h1>
+        <div>
+          <p className={`mt-[41px] text-[18px] text-center align-center ${NeueMachinaRegular.className}`}>20yo. <a href='https://www.itba.edu.ar/' rel="noopener noreferrer" target="_blank" >Computer Science & Engineering @ITBA</a></p>
+          <p className={`mt-[10px] text-[18px] text-center align-center ${NeueMachinaRegular.className}`}>Prev: <a href='https://www.belo.app/' rel="noopener noreferrer" target="_blank" >Belo</a> and <a href='https://www.suku.world/' rel="noopener noreferrer" target="_blank" >Suku</a></p>
+        </div>
         <AvatarLogo className="justify-center flex mt-[71px]" />
         <p className={`hidden md:flex text-[3.5rem] text-blue mt-auto ${NeueMachinaUltraBold.className}`}>[ BLOG ]</p>
-        <div className="md:hidden bg-blue mt-auto mx-[-16px] py-8" >
+        <div className="md:hidden bg-blue mx-[-16px] py-8 mt-[20px]" >
           <p className={`text-[3.5rem] text-white text-center ${NeueMachinaUltraBold.className}`}>[ BLOG ]</p>
         </div>
       </div>
